7191fd99b455430ff72aabfbb7ad2256848ce00d
madcido <fabowow@gmail.com>
2019-07-08 18:57:10 -0300

    New "random" starting positions

--- src/util.js
+++ src/util.js
@@ -2,13 +2,13 @@ import Ship from './ship';
 
 const shipPositioningData = [
     [
-        { len: 1, x: 0, y: 0, hor: true },
+        { len: 1, x: 1, y: 6, hor: true },
         { len: 1, x: 2, y: 0, hor: true },
-        { len: 2, x: 4, y: 0, hor: true },
-        { len: 2, x: 7, y: 0, hor: true },
-        { len: 3, x: 0, y: 2, hor: true },
-        { len: 3, x: 4, y: 2, hor: true },
-        { len: 4, x: 0, y: 4, hor: false },
+        { len: 2, x: 5, y: 1, hor: true },
+        { len: 2, x: 7, y: 7, hor: true },
+        { len: 3, x: 3, y: 4, hor: true },
+        { len: 3, x: 4, y: 6, hor: false },
+        { len: 4, x: 0, y: 0, hor: false },
     ],
     [
         { len: 1, x: 7, y: 0, hor: true },
@@ -18,6 +18,24 @@ const shipPositioningData = [
         { len: 3, x: 4, y: 1, hor: false },
         { len: 3, x: 0, y: 8, hor: true },
         { len: 4, x: 4, y: 7, hor: true },
+    ],
+    [
+        { len: 1, x: 7, y: 1, hor: false },
+        { len: 1, x: 1, y: 6, hor: true },
+        { len: 2, x: 1, y: 2, hor: false },
+        { len: 2, x: 7, y: 7, hor: false },
+        { len: 3, x: 6, y: 4, hor: true },
+        { len: 3, x: 2, y: 8, hor: true },
+        { len: 4, x: 4, y: 1, hor: false },
+    ],
+    [
+        { len: 1, x: 5, y: 6, hor: true },
+        { len: 1, x: 7, y: 7, hor: false },
+        { len: 2, x: 8, y: 4, hor: false },
+        { len: 2, x: 1, y: 6, hor: true },
+        { len: 3, x: 3, y: 2, hor: false },
+        { len: 3, x: 5, y: 1, hor: false },
+        { len: 4, x: 2, y: 8, hor: true },
     ],
 ]
 
